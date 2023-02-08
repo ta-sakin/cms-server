0d05da72e4f4b65c584dbbe1a6ceb0b106ef53fd
Tahjib Al Sakin <tasakin98@gmail.com>
2023-02-08 22:23:57 +0600

    deployed to render

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const xss = require("xss-clean");
 const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
 app.set("trust proxy", 1);
-app.use(
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  })
-);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(helmet());
